3d5ab7058d4d29ae7e7fae79940ab11f7d598cfd
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-01-16 20:50:53 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10)
+
+  if(left > 0 ){
+    dodger.style.left =`${left -1}px`
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10)
+
+    if(left < 1000 ){
+      dodger.style.left =`${left +1}px`
+}
+}
+
+document.addEventListener('keydown', function(){
+  if(e.key=== 'ArrowLeft'){
+    moveDodgerLeft()
+  }else if(e.key=== 'ArrowRight'){
+    moveDodgerRight()
+  }
+})
